3c8ee4cfc342e82774b1e1dca435568ccb4fd8d5
chenziyi <chziy429@163.com>
2017-09-08 13:38:30 +0800

    Update main.sh

--- webserver/main.sh
+++ webserver/main.sh
@@ -17,6 +17,11 @@ filename=$5
 # Number of threads used
 thread=$6
 
+# Directory to the scripts
+script_path=$7
+
+cd ${script_path}
+
 if [ ! -d "${result_path}/01fastq"]; then
 mkdir ${result_path}/01fastq
 fi
@@ -45,28 +50,23 @@ if [ ! -d "${result_path}/new_fastqc" ]; then
 mkdir ${result_path}/new_fastqc
 fi
 
-cd /gluster/home/chenziyi/test/scripts
-
-## gunzip the files
-#sh 01gunzip.sh ${input_path} ${result_path}/01fastq
-
 ## quality control
-#sh 02qc.sh PE ${result_path} ${software} $thread
+sh ${script_path}/02qc.sh PE ${result_path} ${software} $thread
 
 ## mapping
-sh 03mapping.sh PE ${result_path} ${software} ${ref} $thread
+sh ${script_path}/03mapping.sh PE ${result_path} ${software} ${ref} $thread
 
 ## sorting
-sh 04samtools.sh ${result_path} ${software} $thread
+sh ${script_path}/04samtools.sh ${result_path} ${software} $thread
 
 ## strandness
-sh 05-1.strand.sh ${result_path} ${result_path}/StrandInfo ${software} ${ref}
-Rscript 05-2.RSEQc.stat.R ${result_path}/StrandInfo ${result_path}/strand.txt
+sh ${script_path}/05-1.strand.sh ${result_path} ${result_path}/StrandInfo ${software} ${ref}
+Rscript ${script_path}/05-2.RSEQc.stat.R ${result_path}/StrandInfo ${result_path}/strand.txt
 
 ## quantification
-sh 06htseq ${result_path} ${result_path}/strand.txt ${software} ${ref}
+sh ${script_path}/06htseq ${result_path} ${result_path}/strand.txt ${software} ${ref}
 
 ## normalization
-Rscript MouseHTSeq_counts_stat.R ${result_path}/05htseq ${result_path} ${filename}
+Rscript ${script_path}/MouseHTSeq_counts_stat.R ${result_path}/05htseq ${result_path} ${filename}
 
 
