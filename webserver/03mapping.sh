3f10630a627ee92172fed56e270cc5af6e0143b2
chen ziyi <chziy429@163.com>
2018-06-13 09:17:19 +0800

    Update 03mapping.sh

--- webserver/03mapping.sh
+++ webserver/03mapping.sh
@@ -2,63 +2,75 @@
 # PE for Paired end sequencing and SE for Singlen end sequencing
 library_layout=$1
 
-# Directory of the RNASeq result
-result_path=$2
+# Directory of the trimmed fastq file
+input_path=$2
 
-## Directory to the software
-software=$3
+# Directory of the mapping result
+output_path=$3
 
-# Directory to the refernce file
-ref=$4
+## Directory to STAR software
+star=$4
+
+# Directory to the STAR refernce file
+star_ref=$5
 
 # number of threads
-thread=$5
+thread=$6
 
-star=${software}/STAR/bin/Linux_x86_64/STAR
-star_ref=${ref}/mm10/star_v2
-
-gtf=${ref}/mm10/New_Mus_musculus.GRCm38.83.V2.gtf
-genome=${ref}/mm10/Mus_musculus.GRCm38.dna.primary_assembly.83.fa
-
-#####################################################################################################
-# 
-if [ ! -d "${star_ref}" ]; then
-mkdir ${star_ref}
-$star --runThreadN ${thread} \
-      --runMode genomeGenerate \
-      --genomeDir ${star_ref} \
-      --genomeFastaFiles ${genome} \
-      --sjdbGTFfile ${gtf} \
-      --sjdbOverhang 100
-fi
+trimm_path=${input_path}
+mapping_path=${output_path}
 
 #####################################################################################################
 ## Mapping
 if [ "$library_layout" = "PE" ];then
-trimm_path=${result_path}/02trimmed
-mapping_path=${result_path}/03mapping
-
-for i in $(ls ${trimm_path}/*trimm_1P)
+for i in $(ls ${input_path}/*trimm_1P)
 do
 sample_name=`basename $i|sed 's/_trimm_1P//'`
 echo ${sample_name}
-mkdir ${mapping_path}/${sample_name}
-$star --runMode alignReads --genomeDir ${star_ref} --runThreadN ${thread} --readFilesIn ${trimm_path}/${sample_name}_trimm_1P ${trimm_path}/${sample_name}_trimm_2P --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --limitBAMsortRAM 99000000000000 --outSAMunmapped Within --outFilterMultimapNmax 1000 --outFilterMismatchNmax 999 --outSAMtype BAM Unsorted --quantMode TranscriptomeSAM --outFileNamePrefix ${mapping_path}/${sample_name}/${sample_name}
+mkdir ${output_path}/${sample_name}
+$star --runMode alignReads \
+      --genomeDir ${star_ref} \
+      --runThreadN ${thread} \
+      --readFilesIn ${input_path}/${sample_name}_trimm_1P ${input_path}/${sample_name}_trimm_2P \
+      --alignIntronMin 20 \
+      --alignIntronMax 1000000 \
+      --alignMatesGapMax 1000000 \
+      --alignSJoverhangMin 8 \
+      --alignSJDBoverhangMin 1 \
+      --limitBAMsortRAM 99000000000000 \
+      --outSAMunmapped Within \
+      --outFilterMultimapNmax 1000 \
+      --outFilterMismatchNmax 999 \
+      --outSAMtype BAM Unsorted \
+      --quantMode TranscriptomeSAM \
+      --outFileNamePrefix ${output_path}/${sample_name}/${sample_name}
 done
 
 #####################################################################################################
 elif [ "$library_layout" = "SE" ];then
-trimm_path=${result_path}/02trimmed
-mapping_path=${result_path}/03mapping
-
-for i in $(ls ${trimm_path}/*.fastq)
+for i in $(ls ${input_path}/*trimm)
 do
 sample_name=`basename $i|sed 's/_trimm//'`
 echo ${sample_name}
-mkdir ${mapping_path}/${sample_name}
-$star --runMode alignReads --genomeDir ${star_ref} --runThreadN ${thread} --readFilesIn ${trimm_path}/${sample_name}_trimm --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --limitBAMsortRAM 99000000000 --outSAMunmapped Within --outFilterMultimapNmax 1000 --outFilterMismatchNmax 999 --outSAMtype BAM Unsorted --quantMode TranscriptomeSAM --outFileNamePrefix ${mapping_path}/${sample_name}/${sample_name}
+mkdir ${output_path}/${sample_name}
+$star --runMode alignReads \
+      --genomeDir ${star_ref} \
+      --runThreadN ${thread} \
+      --readFilesIn ${input_path}/${sample_name}_trimm \
+      --alignIntronMin 20 \
+      --alignIntronMax 1000000 \
+      --alignMatesGapMax 1000000 \
+      --alignSJoverhangMin 8 \
+      --alignSJDBoverhangMin 1 \
+      --limitBAMsortRAM 99000000000 \
+      --outSAMunmapped Within \
+      --outFilterMultimapNmax 1000 \
+      --outFilterMismatchNmax 999 \
+      --outSAMtype BAM Unsorted \
+      --quantMode TranscriptomeSAM \
+      --outFileNamePrefix ${output_path}/${sample_name}/${sample_name}
 done
-
+echo "mapping finished!"
 #####################################################################################################
 else
 cd ~
