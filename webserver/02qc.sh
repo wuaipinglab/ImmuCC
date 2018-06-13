f56fe92ddaff6cd0d10f82a32e59efbef2099bdd
chen ziyi <chziy429@163.com>
2018-06-13 09:13:21 +0800

    Update 02qc.sh

--- webserver/02qc.sh
+++ webserver/02qc.sh
@@ -1,48 +1,60 @@
+
+
+Note: For paired end sequencing data, the raw fastq files should be ended with *_1.fastq and *_2.fastq
+
 # PE for Paired end sequencing and SE for Singlen end sequencing
 library_layout=$1
 
-# Directory of the RNASeq result
-result_path=$2
+# Directory of the raw fastq files
+fastq_path=$2
 
-## Directory to the software
-software=$3
+# Directory of the trimmed fastq file
+trimm_path=$3
+
+# Directory to the software of fastqc
+fastqc=$4
+
+# Directory to the software of trimmomatic
+trimmomatic=$5
+
+# Directory to the trimmomatic adapters
+adapter=$6
+
+# Directory to the result of raw fastqc result
+raw_fastqc=$7
+
+# Directory to the result of new fastqc result
+new_fastqc=$8
 
 # number of threads
-thread=$4
+thread=$9
 
-adapter=${software}/Trimmomatic-0.35/adapters/TruSeq3-PE.fa
 software_para="2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:10"
-fastqc=${software}/FastQC/fastqc
-trim=${software}/Trimmomatic-0.35/trimmomatic-0.35.jar
 
 #####################################################################################################
 if [ "$library_layout" = "PE" ];then
 
-fastq_path=${result_path}/01fastq
-trimm_path=${result_path}/02trimmed
-
-for i in $(ls ${fastq_path}/*_R1.fastq)
+for i in $(ls ${fastq_path}/*_1.fastq)
 do
-##sample_name=`basename $i|awk -F"_" '{print $1}'`
-sample_name=`basename $i|sed 's/_R1.fastq//'`
+sample_name=`basename $i|awk -F"_" '{print $1}'`
+sample_end=`basename $i|awk -F"_" '{print $2}'`
+#sample_name=`basename $i|sed 's/_1.fastq//'`
 echo ${sample_name}
 
 ## Quality stat of raw samples
-$fastqc ${fastq_path}/${sample_name}_R1.fastq -t ${thread} -o ${result_path}/raw_fastqc 
-$fastqc ${fastq_path}/${sample_name}_R2.fastq -t ${thread} -o ${result_path}/raw_fastqc
+$fastqc ${fastq_path}/${sample_name}_1.fastq -t ${thread} -o ${raw_fastqc}
+$fastqc ${fastq_path}/${sample_name}_2.fastq -t ${thread} -o ${raw_fastqc}
 
 ## Remove low quality reads and trimm adapters
-java -jar $trim PE -threads ${thread} ${fastq_path}/${sample_name}_R1.fastq ${fastq_path}/${sample_name}_R2.fastq -baseout ${trimm_path}/${sample_name}_trimm ILLUMINACLIP:$adapter:${software_para}
+java -jar $trimmomatic PE -threads ${thread} ${fastq_path}/${sample_name}_1.fastq ${fastq_path}/${sample_name}_2.fastq -baseout ${trimm_path}/${sample_name}_trimm ILLUMINACLIP:$adapter:${software_para}
 
 ## Quality stat after QC
-$fastqc ${trimm_path}/${sample_name}_trimm_1P -t ${thread} -o ${result_path}/new_fastqc 
-$fastqc ${trimm_path}/${sample_name}_trimm_2P -t ${thread} -o ${result_path}/new_fastqc
+$fastqc ${trimm_path}/${sample_name}_trimm_1P -t ${thread} -o ${new_fastqc} 
+$fastqc ${trimm_path}/${sample_name}_trimm_2P -t ${thread} -o ${new_fastqc}
 done
 
 #####################################################################################################
 elif [ "$library_layout" = "SE" ];then
-fastq_path=${result_path}/01fastq
-trimm_path=${result_path}/02trimmed
 
 for i in $(ls ${fastq_path}/*.fastq)
 do
@@ -52,13 +64,13 @@ echo ${sample_name}
 echo "Quality Control begin"
 
 ## Quality stat of raw samples
-$fastqc ${fastq_path}/${sample_name}.fastq -t ${thread} -o ${result_path}/raw_fastqc 
+$fastqc ${fastq_path}/${sample_name}.fastq -t ${thread} -o ${raw_fastqc}
 
 ## Remove low quality reads and trimm adapters
-java -jar $trim SE -threads ${thread} ${fastq_path}/${sample_name}.fastq ${trimm_path}/${sample_name}_trimm ILLUMINACLIP:$adapter:${software_para}
+java -jar $trimmomatic SE -threads ${thread} ${fastq_path}/${sample_name}.fastq ${trimm_path}/${sample_name}_trimm ILLUMINACLIP:$adapter:${software_para}
 
 ## Quality stat after QC
-$fastqc ${trimm_path}/${sample_name}_trimm -t ${thread} -o ${result_path}/new_fastqc
+$fastqc ${trimm_path}/${sample_name}_trimm -t ${thread} -o ${new_fastqc}
 
 done
 echo "RNASeq finished"
