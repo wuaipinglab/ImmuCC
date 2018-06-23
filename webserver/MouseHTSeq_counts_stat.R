d21d2cf898a68a1b4df09d0a83657b583a9ef708
chen ziyi <chziy429@163.com>
2018-06-23 17:07:24 +0800

    Update and rename HTSeq_stat.R to MouseHTSeq_counts_stat.R

--- webserver/MouseHTSeq_counts_stat.R
+++ webserver/MouseHTSeq_counts_stat.R
@@ -6,21 +6,14 @@
 # Directory to the htseq result
   htseq.path <- as.character(argv[1])
 
-# Directory to the file which contains list of immune receptor genes
-  immuneGene.path <- as.character(argv[2])
-
-# Directory to the final result
-  result.path <- as.character(argv[3])
-  
+  argv <- commandArgs(TRUE)
+  options(stringsAsFactors=F)
 
   #************************************************************************************
-  # Merge multiplt HTSeq results
-
-  htseq_merge <- function(data.path){
+  htseq_merge <- function(){
       # Arguments:
-      #    data.path:path to the individual HTSeq result files  
-    
-      setwd(data.path)
+      #    path:path to the input files  
+
       options(stringsAsFactors=F)
       files <- list.files()
       files <- files[grep("txt", files)]
@@ -32,12 +25,12 @@
           if (ncol(temp)==0) temp <- read.table(file, row.names=1, header=F, sep="\t")
           if (file==files[1]) {
               counts <- temp
-           #    col <- ncol(temp)
-           #    row <- nrow(temp)
+              col <- ncol(temp)
+              row <- nrow(temp)
           } else {
               counts <- cbind(counts, temp[rownames(counts), ])
-           #   col <- c(col, ncol(temp))
-           #   row <- c(row, nrow(temp))
+              col <- c(col, ncol(temp))
+              row <- c(row, nrow(temp))
           }
       }
       colnames(counts) <- gsub("\\.txt", "", files)
@@ -46,8 +39,6 @@
   }
 
   #************************************************************************************
-  # merge the immune receptor gene subtypes
-
   receptor_merge <- function(expression, gene.list=receptor.ensemble.merge){
       # Arguments:
       #    expression:input expression matrix  
@@ -61,7 +52,7 @@
       for (gene in family) {
           gene.temp <- as.character(unlist(gene.list[gene]))
           gene.temp <- intersect(gene.temp, rownames(expression))
-          cat("The family number of ", gene, " is ", length(gene.temp), "\n")
+          cat("The family number of ", gene, " is ",length(gene.temp), "\n")
           if (length(gene.temp) > 1) {
               expression.temp <- apply(expression[gene.temp, ], 2, sum)
           } else {
@@ -75,8 +66,7 @@
   }
 
   #************************************************************************************
-  # quartile normalization
-  quartile <- function(filename, p){
+  quartile <- function(filename, p) {
       # Arguments:
       #    filename:input file name  
       #    p: quantile
@@ -89,7 +79,7 @@
           if (file.exists(filename)) {
               if (grep("csv", filename)==1)      { data <- read.csv(filename, row.names=1)} 
               else if (grep("txt", filename)==1) { data <- read.csv(filename, row.names=1)}
-              else {break}
+              else { break }
           }
       }
 
@@ -97,8 +87,7 @@
       result <- matrix(nrow=nrow(data), ncol=0)
       for (i in seq(n)) {
           expres <- as.numeric(data[, i])
-          expres.min <- min(expres)
-          value <- expres[expres != expres.min]
+          value <- expres[expres!=0]
           value <- sort(value, decreasing=T)
           value.quantile <- quantile(value, p)
           scale <- as.numeric(value.quantile)/1000
@@ -111,24 +100,33 @@
       result
   }
 
+
+
   #################################################################################
 
-  # 
-  counts.raw <- htseq_merge(htseq.path)
-  name <- "MouseTissue"
-  write.csv(counts.raw, paste(result.path, name, ".Raw.HTSeqData.csv", sep=""), row.names=T)
+  data.path <- as.character(argv[1])
+  receptor.ensemble.merge <- as.character(argv[2])
+  result.path <- as.character(argv[3])
+
+  setwd(data.path)
+  if ("receptor.ensemble.merge.RData" %in% list.files()) {
+      load(receptor.ensemble.merge)
+  } else {
+      cat("receptor.ensemble.merge.RData was not existed in the directory, please input the right path or download the file again!", "\n")
+  }
+  setwd(data.path)
+
+  counts.raw <- htseq_merge()
+  #write.csv(counts.raw, paste(name, ".HTSeqData.csv", sep=""), row.names=T, quote=F)
+
   n <- nrow(counts.raw)
-  index <- grep("ENS", rownames(index))
-  counts.raw <- counts.raw[index, ]
+  gene <- grep("ENS", rownames(counts.raw))
+  counts.raw <- counts.raw[gene, ]
 
-  #
-  load(immuneGene.path)
   counts.merge <- receptor_merge(counts.raw, gene.list=receptor.ensemble.merge)
-  write.csv(counts.merge, paste(result.path, name, ".HTSeqData.immuereceptorMerged.csv", sep=""), row.names=T)
+  #write.csv(counts.merge, paste(name, ".HTSeqData.immuereceptor.csv", sep=""), row.names=T, quote=F)
 
-  # normalization
-  # The normaliazed data named "counts.quatile" is the input file needed for our web server
   p <- 0.75
   counts.quatile <- quartile(counts.merge, p)
-  write.csv(counts.quatile, paste(result.path, name, ".HTSeqData.immuereceptorMerged.quartileNorm.csv", sep=""), row.names=T)
-  
+  write.csv(counts.quatile, paste(result.path, "/HTSeqData.immuereceptor.quartileNorm.csv", sep=""), row.names=T, quote=F)
+
