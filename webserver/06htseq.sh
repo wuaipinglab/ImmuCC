1ff15233baae1a5ac1cc874154dc3444837ff025
chen ziyi <chziy429@163.com>
2019-07-18 09:26:14 +0800

    Update 06htseq.sh

--- webserver/06htseq.sh
+++ webserver/06htseq.sh
@@ -22,7 +22,6 @@ for i in $(ls ${input_path}/*.bam)
 do
 sample_name=`basename $i | sed 's/.bam//'`
 strand=`cat $strand_file|grep $sample_name|awk -F" " '{print $4}'`
-#echo "python $htseq -f bam -t gene -m union -s $strand ${input_path}/${sample_name}.bam ${gtf} >${output_path}/${sample_name}.txt"
 python $htseq -f bam -t gene -m union -s $strand ${input_path}/${sample_name}.bam ${gtf} >${output_path}/${sample_name}.txt
 done  
 
